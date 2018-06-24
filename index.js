a8190844ff287fc9f96fb6e496ba861cf90e66cd
Yu hua An <Owen@Yus-MacBook-Air.local>
2018-06-24 12:46:10 +0800

    Done with all work

---
+++ index.js
@@ -0,0 +1,17 @@
+function submit(){
+    window.location.href = "schedule.html";
+}
+
+
+function submit1(){
+    window.open ("languagetest.html");
+}
+
+function submit2(){
+    window.location.href = "classroom.html";
+}
+
+function buttonbig(){
+    window.open ("languagetest.html");
+}
+
